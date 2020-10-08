c0e5861a35cab003f7f29f3f002b6cbcf6c1a537
Guillaume Subiron <maethor@subiron.org>
2020-10-08 09:44:12 +0200

    Rewrite the module Some history has been lost in a git mistake, but this rewrite - Brings huge performance improvements - Doesn't need complex multithreading anymore (tested on 1000 hosts and 20000 services) - Avoid race conditions due to multithreading - Compatible pymongo3 and mongo replicas
    Just works. But break compatibility with old format so retention will be
    lost (sorry about that, not my choice).

--- module/module.py
+++ module/module.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
+
 # Copyright (C) 2009-2012:
 #    Gabes Jean, naparuba@gmail.com
 #    Gerhard Lausser, Gerhard.Lausser@consol.de
 #    Gregory Starck, g.starck@gmail.com
 #    Hartmut Goebel, h.goebel@goebel-consult.de
+#    Frederic Mohier, frederic.mohier@gmail.com
+#    Guillaume Subiron, guillaume@subiron.org
 #
 # This file is part of Shinken.
 #
@@ -27,229 +29,254 @@ This is a scheduler module to save host/service retention data into a mongodb da
 """
 
 import time
-import cPickle
-import datetime
-
 import base64
-from multiprocessing import Process, cpu_count
+import traceback
+import cPickle
 
 try:
-    from pymongo import MongoClient,version
+    import pymongo
+    from pymongo import MongoClient
+    from pymongo.errors import AutoReconnect, ConnectionFailure, InvalidURI, ConfigurationError, DuplicateKeyError
 except ImportError:
+    logger.error('[Mongodb-Scheduler-Retention] Can not import pymongo and/or MongoClient'
+                 'Your pymongo lib is too old. '
+                 'Please install it with a 3.x+ version from '
+                 'https://pypi.python.org/pypi/pymongo')
     MongoClient = None
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle as pickle
 
 from shinken.basemodule import BaseModule
 from shinken.log import logger
+from shinken.util import to_bool
 
 properties = {
     'daemons': ['scheduler'],
-    'type': 'mongodb_retention',
-    'external': False,
-    }
+    'type': 'retention-mongodb',
+    'external': False
+}
 
-def get_instance(plugin):
-    """
-    Called by the plugin manager to get a broker
-    """
-    logger.debug("Get a Mongodb retention scheduler module for plugin %s" % plugin.get_name())
-    if not MongoClient:
-        raise Exception('Could not use the pymongo module. Please verify your pymongo install.')
-    uri = plugin.uri
-    database = plugin.database
-    username = getattr(plugin, 'username', '')
-    password = getattr(plugin, 'password', '')
-    replica_set = getattr(plugin, 'replica_set', '')
-    instance = Mongodb_retention_scheduler(plugin, uri, database, username, password, replica_set)
+
+# called by the plugin manager to get a mongodb_retention_scheduler instance
+def get_instance(mod_conf):
+    logger.info('[Mongodb-Scheduler-Retention] got an instance of MongodbRetentionScheduler module for %s'
+                % mod_conf.get_name())
+    instance = MongodbRetentionScheduler(mod_conf)
     return instance
 
 
-def chunks(l, n):
-    """ Yield successive n-sized chunks from l.
-    """
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
+class MongodbRetentionSchedulerError(Exception):
+    pass
 
 
-class Mongodb_retention_scheduler(BaseModule):
-    def __init__(self, modconf, uri, database, username, password, replica_set):
-        BaseModule.__init__(self, modconf)
-        self.uri = uri
-        self.database = database
-        self.username = username
-        self.password = password
-        self.replica_set = replica_set
-        self.max_workers = 4
-        # Older versions don't handle replicasets and don't have the fsync option
-        if version < 2:
-            logger.error('[MongodbRetention] Your pymongo lib is too old. '
-                         'Please install at least a 2.x+ version.')
+# Main class
+class MongodbRetentionScheduler(BaseModule):
+
+    def __init__(self, mod_conf):
+        BaseModule.__init__(self, mod_conf)
+
+        self.uri = getattr(mod_conf, 'uri', 'mongodb://localhost')
+        logger.info('[Mongodb-Scheduler-Retention] mongo uri: %s', self.uri)
+
+        self.replica_set = getattr(mod_conf, 'replica_set', None)
+        if self.replica_set and int(pymongo.version[0]) < 3:
+            logger.error('[Mongodb-Scheduler-Retention] Can not initialize module with '
+                         'replica_set because your pymongo lib is too old. '
+                         'Please install it with a 3.x+ version from '
+                         'https://pypi.python.org/pypi/pymongo')
             return None
+
+        self.path = getattr(mod_conf, 'path', None)
+        logger.info('[Mongodb-Scheduler-Retention] old file path: %s', self.path)
+
+        self.database = getattr(mod_conf, 'database', 'shinken')
+        logger.info('[Mongodb-Scheduler-Retention] database: %s', self.database)
+
+        self.hosts_collection_name = getattr(mod_conf, 'hosts_collection_name', 'retention_hosts')
+        logger.info('[Mongodb-Scheduler-Retention] hosts retention collection: %s', self.hosts_collection_name)
+
+        self.services_collection_name = getattr(mod_conf, 'services_collection_name', 'retention_services')
+        logger.info('[Mongodb-Scheduler-Retention] services retention collection: %s', self.services_collection_name)
+
+        self.connection = None
+
+        self.properties = properties
 
 
     def init(self):
         """
-        Called by Scheduler to say 'let's prepare yourself guy'
+        Called by Scheduler to do init work
         """
-        logger.debug("Initialization of the mongodb module")
+        return True
 
-        if self.replica_set:
-            self.con = MongoClient(self.uri, replicaSet=self.replica_set, fsync=False)
-        else:
-            self.con = MongoClient(self.uri, fsync=False)
 
-        #self.con = Connection(self.uri)
-        # Open a gridfs connection
-        self.db = getattr(self.con, self.database)
-        if self.username != '' and self.password != '':
-            self.db.authenticate(self.username, self.password)
-        self.hosts_fs = getattr(self.db, 'retention_hosts_raw') #GridFS(self.db, collection='retention_hosts')
-        self.services_fs = getattr(self.db, 'retention_services_raw')
+    def _open_db(self):
+        """
+        Connect to the Mongo DB with configured URI.
 
-    def job(self, all_data, wid, offset):
+        Execute a command to check if connected on master to activate immediate connection to
+        the DB because we need to know if DB server is available.
+        """
+        self._close_db()
+
+        self.connection = MongoClient(self.uri, connect=False)
+        logger.info("[Mongodb-Scheduler-Retention] trying to connect MongoDB: %s", self.uri)
         try:
-            self._job(all_data, wid, offset)
-        except Exception, exc:
-            logger.error(exc)
-        finally:
-            # I'm a subprocess and need to close my forked socket
-            self.con.close()
+            result = self.connection.admin.command("ismaster")
+            logger.info("[Mongodb-Scheduler-Retention] connected to MongoDB, admin: %s", result)
+            logger.info("[Mongodb-Scheduler-Retention] server information: %s", self.connection.server_info())
 
-    def _job(self, all_data, wid, offset):
-        t0 = time.time()
-        # Reinit the mongodb connection if need
-        self.init()
-        all_objs = {'hosts':{}, 'services':{}}
-        date = datetime.datetime.utcnow()
+            self.db = self.connection[self.database]
+            logger.info("[Mongodb-Scheduler-Retention] connected to the database: %s (%s)", self.database, self.db)
+            self.hosts_collection = self.db[self.hosts_collection_name]
+            self.services_collection = self.db[self.services_collection_name]
 
-        hosts = all_data['hosts']
-        services = all_data['services']
+            logger.info('[Mongodb-Scheduler-Retention] got collections')
+        except ConnectionFailure as e:
+            logger.error("[Mongodb-Scheduler-Retention] Server is not available: %s", str(e))
+            raise MongodbRetentionSchedulerError
+        except (InvalidURI, ConfigurationError):
+            logger.error('[Mongodb-Scheduler-Retention] Mongodb connection URI error: %s' % self.uri)
+            raise MongodbRetentionSchedulerError
+        except Exception as e:
+            logger.error("[Mongodb-Scheduler-Retention] Could not open the database", str(e))
+            raise MongodbRetentionSchedulerError
 
-        # Prepare the encoding for all managed hosts
-        i = -1
-        for h_name in hosts:
-            # Only manage the worker id element of the offset (number of workers)
-            # elements
-            i += 1
-            if (i % offset) != wid:
-                continue
-            h = hosts[h_name]
-            key = "HOST-%s" % h_name
-            val = cPickle.dumps(h, protocol=cPickle.HIGHEST_PROTOCOL)
-            val2 = base64.b64encode(val)
-            # We save it in the Gridfs for hosts
-            all_objs['hosts'][key] = {'_id':key, 'value':val2, 'date':date}
 
-        i = -1
-        for (h_name, s_desc) in services:
-            i += 1
-            # Only manage the worker id element of the offset (number of workers)
-            # elements
-            if (i % offset) != wid:
-                continue
-            s = services[(h_name, s_desc)]
-            key = "SERVICE-%s,%s" % (h_name, s_desc)
-            # space are not allowed in a key.. so change it by SPACE token
-            key = key.replace(' ', 'SPACE')
-            val = cPickle.dumps(s, protocol=cPickle.HIGHEST_PROTOCOL)
-            val2 = base64.b64encode(val)
-            all_objs['services'][key] = {'_id':key, 'value':val2, 'date':date}
+    def _close_db(self):
+        """
+        Close database connection
+        """
+        if self.connection:
+            self.connection.close()
+            self.connection = None
+        logger.info('[Mongodb-Scheduler-Retention] database connection closed')
 
-        if len(all_objs['hosts']) != 0:
-            # Do bulk insert of 500 elements (~500K insert)
-            lsts = list(chunks(all_objs['hosts'].values(), 500))
-            for lst in lsts:
-                bulk=self.hosts_fs.initialize_unordered_bulk_op()
-                for doc in lst:
-                    bulk.find({'_id': doc['_id']}).upsert().replace_one(doc)
-                res = bulk.execute({'w': 0})
 
-        if len(all_objs['services']) != 0:
-            # Do bulk insert of 500 elements (~500K insert)
-            lsts = list(chunks(all_objs['services'].values(), 500))
-            for lst in lsts:
-                bulk = self.services_fs.initialize_unordered_bulk_op()
-                for doc in lst:
-                    bulk.find({'_id': doc['_id']}).upsert().replace_one(doc)
-                res = bulk.execute({'w': 0})
+    def hook_load_retention(self, daemon):
+        """
+        Called by Scheduler to restore stored retention data
+        """
+        # Load retention data from a previous retention using flat file
+        # Useful to migrate from flat file retention to MongoDB retention
+        if self.path:
+            logger.info("[Mongodb-Scheduler-Retention] Reading from retention_file %s" % self.path)
+            try:
+                f = open(self.path, 'rb')
+                all_data = cPickle.load(f)
+                f.close()
+            except (EOFError, ValueError, IOError) as exp:
+                logger.warning("[Mongodb-Scheduler-Retention] error reading retention file: %s" % str(exp))
+                return False
+            except (IndexError, TypeError) as exp:
+                logger.warning("[Mongodb-Scheduler-Retention] Sorry, the resource file is not compatible!")
+                return False
 
-        # Return and so quit this sub-process
-        return
+            # call the scheduler helper function for restoring values
+            daemon.restore_retention_data(all_data)
+
+            logger.info("[Mongodb-Scheduler-Retention] Retention objects loaded successfully.")
+            return
+
+        try:
+            self._open_db()
+        except Exception:
+            logger.warn("[Mongodb-Scheduler-Retention] retention load error")
+            return
+
+        now = time.time()
+        logger.info('[Mongodb-Scheduler-Retention] start loading hosts and services retention…')
+        hosts = {}
+        services = {}
+        restored_hosts = {}
+        restored_services = {}
+        try:
+            host_cursor = self.hosts_collection.find()
+            service_cursor = self.services_collection.find()
+            for host in host_cursor:
+                value = host.get('value')
+                restored_hosts[host.get('_id')] = value
+            for service in service_cursor:
+                value = service.get('value')
+                restored_services[service.get('_id')] = value
+            for host in daemon.hosts:
+                key = '%s,hostcheck' % (host.host_name)
+                if key in restored_hosts:
+                    restored_value = restored_hosts[key]
+                    value = pickle.loads(base64.b64decode(restored_value))
+                    hosts[host.host_name] = value
+                    logger.debug('[Mongodb-Scheduler-Retention] restored host retention: %s' % (key))
+            for service in daemon.services:
+                key = '%s,%s' % (service.host.host_name, service.service_description)
+                if key in restored_services:
+                    restored_value = restored_services[key]
+                    value = pickle.loads(base64.b64decode(restored_value))
+                    services[(service.host.host_name,service.service_description)] = value
+                    logger.debug('[Mongodb-Scheduler-Retention] restored service retention: %s.' % (key))
+
+            logger.info("[Mongodb-Scheduler-Retention] Sending %d hosts and %d services to scheduler restore_retention_data()", len(hosts), len(services))
+            daemon.restore_retention_data({'hosts': hosts, 'services': services})
+
+        except Exception:
+            logger.error('[Mongodb-Scheduler-Retention] Retention load error.')
+            logger.error('[Mongodb-Scheduler-Retention] %s'
+                        % traceback.format_exc())
+
+        logger.info("[Mongodb-Scheduler-Retention] loaded %d hosts and %d services in %3.3fs", len(hosts), len(services), time.time() - now)
+        self._close_db()
 
 
     def hook_save_retention(self, daemon):
         """
-        main function that is called in the retention creation pass
+        Called by Scheduler to save data
         """
-
+        retention = daemon.get_retention_data()
         try:
-            self.max_workers = cpu_count()
-        except NotImplementedError:
-            pass
+            self._open_db()
+        except Exception:
+            logger.warn("[Mongodb-Scheduler-Retention] retention save error")
+            return
 
-        t0 = time.time()
-        logger.debug("[MongodbRetention] asking me to update the retention objects")
+        # Hosts / services retention
+        now = time.time()
+        hosts = retention['hosts']
+        services = retention['services']
+        logger.info('[Mongodb-Scheduler-Retention] start saving retention for %d hosts and %d services…', len(hosts), len(services))
+        try:
+            for host in hosts:
+                _id = '%s,hostcheck' % host
+                logger.debug('[Mongodb-Scheduler-Retention] saving host retention: %s.' % host)
+                host_retention = hosts[host]
+                dumped_value = pickle.dumps(host_retention, protocol=pickle.HIGHEST_PROTOCOL)
+                value = base64.b64encode(dumped_value)
+                self.hosts_collection.remove({'_id': _id})
+                retention_data = {'_id': _id,
+                                  'value': value,
+                                  'timestamp': int(time.time())
+                                  }
+                self.hosts_collection.insert(retention_data)
+            logger.info('[Mongodb-Scheduler-Retention] saved %d hosts retention.', len(hosts))
 
-        all_data = daemon.get_retention_data()
+            for (host, service) in services:
+                _id = '%s,%s' % (host, service)
+                logger.debug('[Mongodb-Scheduler-Retention] saving service retention: %s.' % _id)
+                service_retention = services[(host, service)]
+                dumped_value = pickle.dumps(service_retention, protocol=pickle.HIGHEST_PROTOCOL)
+                value = base64.b64encode(dumped_value)
+                self.services_collection.remove({'_id': _id})
+                retention_data = {'_id': _id,
+                                  'value': value,
+                                  'timestamp': int(time.time())
+                                  }
+                self.services_collection.insert(retention_data)
+            logger.info('[Mongodb-Scheduler-Retention] saved %d services retention.', len(services))
+        except Exception:
+            logger.error('[Mongodb-Scheduler-Retention] error saving hosts/services retention: %s'
+                        % traceback.format_exc())
 
-        processes = []
-        for i in xrange(self.max_workers):
-            proc = Process(target=self.job, args=(all_data, i, self.max_workers))
-            proc.start()
-            processes.append(proc)
+        logger.info("[Mongodb-Scheduler-Retention] saved %d hosts and %d services in %3.3fs", len(hosts), len(services), time.time() - now)
 
-        # Allow 30s to join the sub-processes, should be enough
-        for proc in processes:
-            proc.join(30)
-
-        logger.info("Retention information updated in Mongodb (%.2fs)" % (time.time() - t0))
-
-
-    # Should return if it succeed in the retention load or not
-    def hook_load_retention(self, daemon):
-
-        # Now the new redis way :)
-        logger.debug("MongodbRetention] asking me to load the retention objects")
-
-        # We got list of loaded data from retention uri
-        ret_hosts = {}
-        ret_services = {}
-
-        found_hosts = {}
-        found_services = {}
-
-        for h in self.hosts_fs.find():
-            val = h.get('value', None)
-            if val is not None:
-                found_hosts[h.get('_id')] = val
-
-        for s in self.services_fs.find():
-            val = s.get('value', None)
-            if val is not None:
-                found_services[s.get('_id')] = val
-
-        for h in daemon.hosts:
-            key = "HOST-%s" % h.host_name
-            if key in found_hosts:
-                val = found_hosts[key]
-                val = base64.b64decode(val)
-                val = cPickle.loads(val)
-                ret_hosts[h.host_name] = val
-
-        for s in daemon.services:
-            key = "SERVICE-%s,%s" % (s.host.host_name, s.service_description)
-            # space are not allowed in memcache key.. so change it by SPACE token
-            key = key.replace(' ', 'SPACE')
-            if key in found_services:
-                val = found_services[key]
-                val = base64.b64decode(val)
-                val = cPickle.loads(val)
-                ret_services[(s.host.host_name, s.service_description)] = val
-
-        all_data = {'hosts': ret_hosts, 'services': ret_services}
-
-        # Ok, now comme load them scheduler :)
-        daemon.restore_retention_data(all_data)
-
-        logger.info("[MongodbRetention] Retention objects loaded successfully.")
-
-        return True
+        self._close_db()
